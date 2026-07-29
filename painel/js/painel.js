abf4ddec5fde61c939a165962a3e2a8fb6262ca2
VisionX <raimundoalcantarapinheiro@gmail.com>
2026-07-29 15:34:49 -0300

    Adiciona icones circulares de redes sociais no meio do footer
    Alem do bloco de infos de contato ja existente no canto direito
    (que fica com texto: telefone, email e @insta), agora o footer tem
    um bloco central com 3 icones circulares clicaveis:
    
    - WhatsApp (verde no hover)
    - Instagram (rosa no hover)
    - E-mail (dourado no hover)
    
    Os icones tem area de toque generosa (44x44px), animam em hover
    (sobem 3px + borda dourada) e se reordenam no mobile ficando no
    final do footer.
    
    Aplicado na home e nas 7 paginas de servico. Cache-bust v=8.

--- painel/js/painel.js
+++ painel/js/painel.js
@@ -4,8 +4,8 @@
 
 // Cache-busting: incrementar a versao em toda mudanca em dados.js/config.js
 // para o navegador buscar o arquivo novo, ignorando cache HTTP e do disco.
-import { MODO_DEMO } from "./config.js?v=7";
-import * as dados from "./dados.js?v=7";
+import { MODO_DEMO } from "./config.js?v=8";
+import * as dados from "./dados.js?v=8";
 
 // ---------------------------------------------------------------------
 // Atalhos e utilitarios
